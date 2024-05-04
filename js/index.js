32029ced8483be745b47b236a73874fb10254961
DELLVOSTRO\phamk <phamkhuong436@gmail.com>
2024-05-04 16:01:13 +0700

    add bonus

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var a = document.getElementsByClassName("user");
+// sự kiện khi click vào thẻ a
+a.onclick = function () {
+  a.style.color = "red";
+  var optionsDiv = document.getElementById("options");
+  // hủy display none
+  
+//   optionsDiv.classList.toggle("hidden");
+};
